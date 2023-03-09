3478b01f663ed0a1cc589ac529e44ff82fc05244
vshngpl007 <vshngpl007@gmail.com>
2023-03-09 10:29:43 +0530

    changes

--- src/specs/TestSpec001.js
+++ src/specs/TestSpec001.js
@@ -3,8 +3,6 @@ import { expect } from 'chai';
 
 describe('Login', () => {
   it('should login successfully', () => {
-    LoginPage.open().login('tomsmith', 'SuperSecretPassword!');
-    expect(LoginPage.getMessage()).to.includes('You logged into a secure area!');
-    browser.pause(5000);
+    browser.url('https://www.google.com');
   });
 });
