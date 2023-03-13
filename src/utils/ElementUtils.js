39f11b6c7e8be711cd19c7a894b77d4c17a6ef5b
vshngpl007 <vshngpl007@gmail.com>
2023-03-13 18:01:33 +0530

    changes

--- src/utils/ElementUtils.js
+++
@@ -1,9 +0,0 @@
-class ElementUtils {
-
-  element(selector, timeout = 30000) {
-    browser.$(selector).waitForDisplayed(timeout, false, 'Element not found ' + selector);
-    return browser.$(selector);
-  }
-}
-
-module.exports = new ElementUtils();
